c427450ba58ddfa06985770b83ad95e4b6df12ec
Baramasimbe <bbigwel@gmail.com>
2024-02-22 12:29:54 +0200

    changed

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import './testimonials.css'
 import TEAM from '../../assets/profile0.jpg'
 
 
 import 'swiper/swiper-bundle.css'; // Import the complete Swiper styles
 
-import SwiperCore, { Pagination } from 'swiper';
+import SwiperCore, { Pagination ,Autoplay,Navigation } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-SwiperCore.use([Pagination]);
+
+SwiperCore.use([Pagination, Autoplay, Navigation]); // Include Autoplay in SwiperCore.use
+
 
 
 const Testimonials = () => {
+
+  const progressCircle = useRef(null);
+  const progressContent = useRef(null);
+  const onAutoplayTimeLeft = (s, time, progress) => {
+    progressCircle.current.style.setProperty('--progress', 1 - progress);
+    progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
+  };
+
   return (
     <section id="testimonials">
       <h5>Review from my team</h5>
@@ -21,11 +31,19 @@ const Testimonials = () => {
         className="container testimonials_container"
         spaceBetween={40}
         slidesPerView={1}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        navigation={true}
         pagination={{ clickable: true }}
       >
         <SwiperSlide className="testimonial">
           <div className="team_avatar">
-            <img src={TEAM} alt="" />
+          <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+</svg>
           </div>
           <h5 className="team_name">Phiri Lesly</h5>
           <small className="team_review">
@@ -36,8 +54,11 @@ const Testimonials = () => {
         </SwiperSlide>
 
         <SwiperSlide className="testimonial">
-          <div className="team_avatar">
-            <img src={TEAM} alt="" />
+        <div className="team_avatar">
+          <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+</svg>
           </div>
           <h5 className="team_name">Nyamwena Elister</h5>
           <small className="team_review">
@@ -48,8 +69,11 @@ const Testimonials = () => {
         </SwiperSlide>
 
         <SwiperSlide className="testimonial">
-          <div className="team_avatar">
-            <img src={TEAM} alt="" />
+        <div className="team_avatar">
+          <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+</svg>
           </div>
           <h5 className="team_name">Munemo Edmore</h5>
           <small className="team_review">
@@ -59,8 +83,11 @@ const Testimonials = () => {
           </small>
         </SwiperSlide>
         <SwiperSlide className="testimonial">
-          <div className="team_avatar">
-            <img src={TEAM} alt="" />
+        <div className="team_avatar">
+          <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+</svg>
           </div>
           <h5 className="team_name">Ndou Bismack</h5>
           <small className="team_review">
@@ -68,7 +95,15 @@ const Testimonials = () => {
           has been instrumental in achieving our project milestones. He brings a unique blend of 
           creativity, technical proficiency, and professionalism to every project, making him a standout performer in our software development ecosystem.
           </small>
+          <div className="autoplay-progress">
+    <svg viewBox="0 0 48 48" ref={progressCircle}>
+      <circle cx="24" cy="24" r="20"></circle>
+    </svg>
+    <span ref={progressContent}></span>
+  </div>
         </SwiperSlide>
+
+      
       </Swiper>
     </section>
   );
